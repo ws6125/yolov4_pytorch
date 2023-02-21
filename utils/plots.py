7f5bf3bbf36c602d7e05b4097bdbbe10fae316fd
ws6125 <aws5125@hotmail.com>
2023-02-21 16:36:56 +0800

    To modify type of pred
    To modify type of pred

--- utils/plots.py
+++ utils/plots.py
@@ -95,6 +95,9 @@ def output_to_target(output, width, height):
     for i, o in enumerate(output):
         if o is not None:
             for pred in o:
+                if isinstance(pred, torch.Tensor):
+                    pred = pred.cpu()
+                
                 box = pred[:4]
                 w = (box[2] - box[0]) / width
                 h = (box[3] - box[1]) / height
